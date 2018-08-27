8ab3ced9e0ad2620dc8d5eb23cf5efa57665198b
pavankalyankolla <pkalyan097@gmail.com>
2018-08-27 13:38:03 +0530

    moddleware-setup

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+
+
+
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.json());
+app.use(morgan('dev'));
+
+
+app.listen(port,() => {
+    console.log('listening port',port);
+})
