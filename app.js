28489d71aeeb05c368ea9d95422307854f1fd68d
pavankalyankolla <pkalyan097@gmail.com>
2018-08-30 14:43:47 +0530

    morgan-tokens

--- app.js
+++ app.js
@@ -15,7 +15,13 @@ const port = 3000;
 app.use(useragent.express());
 
 app.use(bodyParser.json());
-app.use(morgan('dev'));
+// app.use(morgan('dev'));
+
+app.use(morgan(function(tokens,req,res){
+    return [ `Started : ${tokens.method(req,res)} : ${tokens.url(req,res)} for ${req.ip} at ${new Date()}
+            Completed : ${tokens.status(req,res)} in  ${tokens['response-time'](req, res)}ms ` ]
+}))
+
 
 app.use('/url',UrlRouter);
 
@@ -24,7 +30,7 @@ app.use(function (req,res,next){
     res.send({
         notice : '404 -The resource you are looking for doesn’t exist'
     })
-})
+});
 app.listen(port,() => {
     console.log('listening port',port);
 })
